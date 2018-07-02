69d60b15d7805948c8b975e9c7a02eec975edab3
xinconan <xinconan2@sina.com>
2018-07-02 13:47:36 +0800

    feat: add wx support

---
+++ DiyImg-wx.vue
@@ -0,0 +1,29 @@
+<template>
+  <img :src="imgSrc" @error="onImgErr" :mode="mode">
+</template>
+
+<script>
+export default {
+  name: 'DiyImg',
+  props: {
+    src: String,
+    errorSrc: String,
+    mode: {
+      type: String,
+      default: 'aspectFit'
+    }
+  },
+  data () {
+    return {
+      imgSrc: this.src
+    }
+  },
+  methods: {
+    onImgErr () {
+      // not supported in miniapp
+      // this.$refs.DiyImg.src = this.errorSrc
+      this.imgSrc = this.errorSrc
+    }
+  }
+}
+</script>
--- DiyImg.vue
+++ DiyImg.vue
@@ -1,9 +1,19 @@
 <template>
-  <img :src="src" alt="" :onerror="errorSrc">
+  <img :src="src" ref="DiyImg" :alt="alt" @error="onImgErr">
 </template>
 
 <script>
 export default {
-  props: ['src', 'errorSrc'],
+  name: 'DiyImg',
+  props: {
+    alt: String,
+    src: String,
+    errorSrc: String
+  },
+  methods: {
+    onImgErr () {
+      this.$refs.DiyImg = this.errorSrc
+    }
+  }
 }
 </script>
